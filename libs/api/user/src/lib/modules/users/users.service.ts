8bab38c9514fe8bd510d41f37dd30b497a152121
Johannes Schobel <johannes.schobel@googlemail.com>
2020-12-18 10:13:26 +0100

    chore: add service

--- libs/api/user/src/lib/modules/users/users.service.ts
+++ libs/api/user/src/lib/modules/users/users.service.ts
@@ -1,26 +1,39 @@
+import { EntityRepository, wrap } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@nx-nest-mikroorm-playground/api/data';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly repository: EntityRepository<UserEntity>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = new UserEntity();
+    user.username = createUserDto.username;
+    return this.repository.persistAndFlush(user)
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.repository.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.repository.findOne(id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.repository.findOne(id);
+    wrap(user).assign(updateUserDto);
+    return this.repository.persistAndFlush(user);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.repository.remove({id: id})
   }
 }
